2db23e6e72fc82d8ddd004c3b0be23d32a655a0c
AdkCoderz <akshaykurveak@gmail.com>
2025-07-20 00:50:21 +0530

    new tab icos

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,12 +1,18 @@
+import FontAwesome5 from '@expo/vector-icons/FontAwesome5';
 import { Tabs } from "expo-router";
 
 export default function TabLayout() {
   return (
-    <Tabs>
-      <Tabs.Screen name="index" options={{ title: 'Home', headerShown: false }} />
-      <Tabs.Screen name="html" options={{ title: 'HTML', headerShown: false }} />
-      <Tabs.Screen name="css" options={{ title: 'CSS', headerShown: false }} />
-      <Tabs.Screen name="js" options={{ title: 'JS', headerShown: false }} />
+    <Tabs
+      screenOptions={{
+        tabBarActiveTintColor: '#00FFCC',   // vibrant purple for active
+        tabBarInactiveTintColor: '#22223B', // dark muted for inactive
+      }}
+    >
+      <Tabs.Screen name="index" options={{ title: 'Home', tabBarIcon: ({ color }) =><FontAwesome5 name="home" size={24} color={color} />, headerShown: false }} />
+      <Tabs.Screen name="html" options={{ title: 'HTML', tabBarIcon: ({ color }) =><FontAwesome5 name="html5" size={24} color={color} />, headerShown: false }} />
+      <Tabs.Screen name="css" options={{ title: 'CSS', tabBarIcon: ({ color }) =><FontAwesome5 name="css3" size={24} color={color} />, headerShown: false }} />
+      <Tabs.Screen name="js" options={{ title: 'JS', tabBarIcon: ({ color }) =><FontAwesome5 name="js" size={24} color={color} />, headerShown: false }} />
     </Tabs>
   );
 }
